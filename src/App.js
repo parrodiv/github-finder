fc531e304138a5b1ab0912c7e84914a2271f056c
Alessandro Parrilla <paggio96@gmail.com>
2022-04-26 16:12:50 +0200

    Setup Github Context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About'; 
+import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={< Home />}/>
-            <Route path='/about' element={< About />}/>
-            <Route path='/notfound' element={< NotFound />}/>
-            <Route path='/*' element={< NotFound />}/>
-            {/*asterisco indica qualsiasi altra cosa */}
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+              {/*asterisco indica qualsiasi altra cosa */}
+            </Routes>
+          </main>
 
-        < Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
